b62776a45c4db22448a3736927c2fa0320c2ff7b
maxwelldu <dcj3sjt@126.com>
2017-08-24 17:51:01 +0800

    add 20170824

---
+++ 20170824/23-拉钩的json.js
@@ -0,0 +1,26 @@
+{
+    "content": {
+        "data": {
+            "approveList": {
+                "147": true,
+                "3450": true,
+                "19477": true,
+                "25439": true,
+                "28362": true,
+                "54359": true,
+                "61921": true,
+                "73666": false,
+                "82991": true,
+                "110502": true,
+                "121656": true,
+                "133382": true,
+                "167443": true,
+                "196329": false,
+                "218519": false
+            }
+        },
+        "rows": []
+    },
+    "message": "操作成功",
+    "state": 1
+}
---
+++ 20170824/README.md
@@ -0,0 +1,6 @@
+#作业：
+- 写一个toast组件，代替alert；参考http://t4t5.github.io/sweetalert/   http://layer.layui.com/  https://weui.io/
+- 通用的表单验证组件（可以在网上找相关的资料进行参考）通用的方法可以是正则验证，相等验证，范围验证，密码验证验证；所有验证相关的可以写到一个JS中，供以后使用；参考http://www.cnblogs.com/lvchenfeng/p/5295823.html
+- http://www.json.org/json-zh.html 读懂了
+- 代码敲一遍
+- 无缝滚动组件，支持左右，上下
